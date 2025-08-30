fad2bf7e5dbef4987b56ec8afbb56cd61daebd9a
Tom Kazakov <tomkazakov@gmail.com>
2025-08-30 17:07:14 -0400

    A0 obstacle is working now

--- A0_obstacle_field.py
+++ A0_obstacle_field.py
@@ -4,9 +4,15 @@
 # Obstacle field: parametric generation for the 128x128 grid filling with tetrominoes without overlap
 # Gen AI usage: ChatGPT to refresh myself on matplotlib and numpy syntax
 
-"""
-=== Assumptions for the exercise
+import random
 
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import signedinteger
+from numpy._typing import _32Bit, _64Bit
+
+# Assumptions for the exercise
+"""
 Based on the Figure 3 as the example output:
 1. Only four tetromino shapes are allowed: straight, L, skew, and T (the square is excluded).
 2. Shapes can be rotated.
@@ -20,39 +26,102 @@ Based on the LaTex listing provided, I'm not entirely sure how rotation and mirr
 But as the listing is referred to as a minimal implementation and not explicitly mentioned as a qualified solution,
 I opt to operate using Figure 3 as the primary guidance.
 
-Coverage clarification: "a coverage rate of 10% would place approximately 400
-obstacles in the field, occupying maybe 1600 cells out of 16384 total"
-This implementation will place 10% or more obstacle cells, relying on the operative
+Coverage clarification: "a coverage rate of 10% would place approximately 400 obstacles in the field, occupying maybe 
+1600 cells out of 16384 total"
+This implementation will place 10% or more obstacle cells (or another specified percentage), relying on the operative
 "approximately 400" in the assignment.
+
+This is not the only way to structure the assignment implementation, but for clarity and to limit the scope,
+these are the guiding principles I will apply.
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
+# Rotate programmatically or list explicitly?
+"""
+I considered three ways of implementing this: 
+1. List all 18 shapes. Silly, but might be actually the most laconic form. And saves tiny amount of compute in-flight.
+2. Pre-compute all mirrors and rotations, but do it algorithmically. Saves on compute, not silly, but needs 
+to be unit-tested or at least validated.
+3. Compute on the fly. Might be the most elegant, but a tiny bit inefficient, and I would have to consider the RNG 
+weights strategy to make sure straight tetrominoes (the fewest non-canonical forms) are not dominating the selection.
 
-# TODO: rotate programmatically or list explicitly?
+Key decision in shape selection is if each one of the 18 resulting shapes should be in the random selection with equal 
+probability (option a). Alternative and valid option is (b): to have an expanded list of canonical shapes which includes 
+mirrored ones, and then treat rotation as a separate in-flight process. 
+
+I chose (b), and the most laconic form is combination of 1 and 3 approached: expanded canonical list + on-the-fly rot. 
+The only tiny downside of this is slight bump in the number of I-tetrominoes being placed. 
+"""
+
 canonical_shapes = (
-    [[1, 1, 1, 1]],  # straight tetromino
-    [[0, 0, 1], [1, 1, 1]],  # left-hand L
-    [[0, 1, 1], [1, 1, 0]],  # skew
-    [[1, 1, 1], [0, 1, 0]]
+    [[1, 1, 1, 1]],  # straight (or I)
+    [[0, 0, 1], [1, 1, 1]],  # L
+    [[1, 0, 0], [1, 1, 1]],  # J
+    [[0, 1, 1], [1, 1, 0]],  # S-skew
+    [[1, 1, 0], [0, 1, 1]],  # Z-skew
+    [[1, 1, 1], [0, 1, 0]]  # T
 )
 
-def rotate(shape: np.ndarray):
-    return np.rot90(shape, k=1)
 
-for _ in range(3):
-    I = rotate(I)
-    print(I)
+def rotate(shape: np.ndarray):
+    """
+    Outputs a random rotation of the given shape, -90 to 180 degrees.
+    """
+    return np.rot90(shape, random.choice([-1, 0, 1, 2]))
+
 
 grid_size = 128
 grid = np.zeros((grid_size, grid_size), dtype=int)
 
-# random filling of the array
-grid[np.random.randint(0, grid_size, 1000), np.random.randint(0, grid_size, 1000)] = 1
+# Strategy to populate the grid
+"""
+The goal is to cover the specified rate of the field. One feasible and laconic way of doing this is to calculate the 
+required number of cells, divide by four per tetromino, and adjust by an empirically found coefficient coming out of 
+a statistically significant number of test runs. This was not a success for me, as the higher the rho goes, the more 
+tetrominoes are overlapped.
 
-# show with matplotlib
-plt.figure(figsize=(10,10))  # image size in inches (ew)
+Another approach is to place a guaranteed minimum number of shapes onto the grid assuming no overlap, and then iterate
+adding more computing the coverage occasionally (batch trickling; fancy) or on every addition (brute force, but simple). 
+
+Another concern is going outside of the field boundaries. This is not valid according to the chosen assumptions. 
+"""
+
+rho = 0.70  # EDIT THIS
+print(f"Target coverage rate: {rho * 100:.2f}%")
+cell_target = int(rho * grid_size * grid_size)
+
+coverage = 0.0
+cell_coverage = 0
+
+while coverage < rho:
+    """
+    Work in batches; each assumes no overlap.
+    """
+    # calculate the batch, place at least one tetromino
+    placements_count = max(1, int((cell_target - cell_coverage) / 4))
+    print(f"Batch placements: {placements_count}")
+
+    for _ in range(placements_count):
+        # get a shape
+        shape = rotate(random.choice(canonical_shapes))
+        mask = np.array(shape)
+
+        # place the shape in a random location using masking
+        mask_width = mask.shape[1]
+        mask_height = mask.shape[0]
+        row, col = np.random.randint(0, grid_size - mask_height + 1), np.random.randint(0, grid_size - mask_width + 1)
+        substitute = grid[row:row + mask_height, col:col + mask_width]
+        substitute[:] = np.maximum(substitute, mask)
+
+    cell_coverage = np.count_nonzero(grid)
+    coverage = cell_coverage / grid_size / grid_size
+    print(f"After-batch coverage: ~{coverage * 100:.2f}%")
+
+# Resulting coverage
+coverage = np.count_nonzero(grid) / grid_size / grid_size
+print(f"Resulting coverage: ~{coverage * 100:.2f}%")
+
+# Show with matplotlib
+plt.figure(figsize=(10, 10))  # image size in inches (ew, why inches)
 plt.imshow(grid, cmap="gray_r")
-plt.axis("off")
-plt.grid(False)
+plt.axis("on")
 plt.show()
